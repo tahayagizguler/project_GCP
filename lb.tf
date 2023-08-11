a3de3a48c3171e212924eba788b953c9d42cf187
tahayagizguler <yagizguler7@gmail.com>
2023-08-11 17:08:22 +0300

    Ansible conf. for jenkins pipeline.

--- lb.tf
+++ lb.tf
@@ -3,7 +3,7 @@ resource "google_compute_health_check" "healthcheck" {
   name = "healthcheck"
   timeout_sec = 1
   check_interval_sec = 1
-  http_health_check {
+  tcp_health_check {
     port = 80
   }
 }
